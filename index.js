b494c2229922ee2dd2d669b3455cca222eea98a5
Javier <javier@kona.tech>
2021-01-31 16:27:11 -0300

    in process

---
+++ index.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+
+async function main() {
+  // Your async code here
+  try {
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    
+    const page = await browser.newPage();
+    // await page.setRequestInterception(true);
+    
+    await page.goto('https://www.pedidosya.com.uy/');
+    await page.type('#address', 'Joaquin Requena 2530');
+    await page.click('#search');
+    await page.waitForTimeout(1000)
+    await page.click('#confirm');
+    await page.waitForTimeout(1000)
+    const getThemAll = await page.$$('a.arrivalName')
+    // console.log(getThemAll)
+    console.log(getThemAll.length)
+    getThemAll.forEach(async link => {
+      // console.log(link)
+      // await page.evaluate(() => link.click())
+    })
+    
+    // await page.goBack()
+  } catch (err) {
+    console.log('Error:', err)
+  }
+}
+
+main();
