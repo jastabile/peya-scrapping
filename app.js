a35c34bce78d150c73f25d2658eda766c9807113
Javier <javier@kona.tech>
2021-03-21 14:10:28 -0300

    facu prueba

---
+++ app.js
@@ -0,0 +1,6 @@
+// Prueba facu: usando Puppeter crear un código que ingrese a Pedidos Ya, 
+//   ingrese tu dirección de la casa y busque restaurantes. (no usar async await en las funciones de puppeter)
+//   Implementar todo el código en una branch nueva (devFacu)
+//   Pushar lo de la branch devFacu
+//   Mergear a dev
+//   Llamada
